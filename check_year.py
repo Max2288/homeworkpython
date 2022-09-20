4493be0f878a13f52c063f0cc8f778b23882ef25
Max2288 <42071821+Max2288@users.noreply.github.com>
2022-09-20 11:48:42 +0300

    Исправлен код, уменьшено количество операторов

--- check_year.py
+++ check_year.py
@@ -1,49 +1,12 @@
-def checker(day:int,month:int,year:int):
-    if month < 1 or month >12:
-        return False
-    if day < 1 or day >31:
-        return False
-    flag = True
-    if year %4 == 0 and year %100!=0 or (year % 100 == 0 and year % 400 == 0):
-        flag = False 
-    if flag == False and month == 2:
-        if day <1 or day >29:
-            return False
-    if month == 1:
-        if day <1 or day>31:
-            return False
-    if month ==2 and flag ==True:
-        if day <1 or day >28:
-            return False
-    if month ==3:
-        if day <1 or day >31:
-            return False
-    if month ==4:
-        if day <1 or day >30:
-            return False
-    if month ==5:
-        if day <1 or day >31:
-            return False
-    if month ==6:
-        if day <1 or day >30:
-            return False
-    if month ==7:
-        if day <1 or day >31:
-            return False
-    if month ==8:
-        if day <1 or day >31:
-            return False
-    if month ==9:
-        if day <1 or day >30:
-            return False
-    if month ==10:
-        if day <1 or day > 31:
-            return False
-    if month ==11:
-        if day <1 or day >30:
-            return False
-    if month ==12:
-        if day <1 or day > 31:
-            return False
-    else:
+def checker(day: int, month: int, year: int):
+    if 0 < day <= 31 and month in [1, 3, 5, 7, 8, 10, 12]:
         return True
+    if 0 < day <= 30 and month in [4, 6, 9, 11]:
+        return True
+    if month == 2:
+        if year % 100 == 0 and year % 400 == 0 or year % 4 == 0 and year % 100 != 0:
+            if 0 < day <= 29:
+                return True
+        if 0 < day <= 28:
+            return True
+    return False
